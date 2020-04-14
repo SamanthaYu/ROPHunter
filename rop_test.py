8715c57d2bb61c09be3c16a3b871d4f14c4d36c2
sya111 <sya111@sfu.ca>
2020-04-13 22:14:55 -0700

    Renamed ROP implementation to ROPHunter

--- rop_test.py
+++ rop_test.py
@@ -1,16 +1,16 @@
 import pygtrie
-from rop import ROPGadget
+from rop import ROPHunter
 import unittest
 
 
 class ROPTest(unittest.TestCase):
     # Uses the example instruction from the paper, "Geometry of Innocent Flesh on the Bone"
     def test_valid_inst(self):
-        rop_gadget = ROPGadget()
+        rop_hunter = ROPHunter()
         start_offset = 0x1000
         code = b"\xc7\x07\x00\x00\x00\x0f\x95\x45\xc3"
 
-        rop_gadget.galileo(start_offset, code)
+        rop_hunter.galileo(start_offset, code)
 
         expected_trie = pygtrie.StringTrie()
         expected_trie["c3"] = "ret"
@@ -21,7 +21,7 @@ class ROPTest(unittest.TestCase):
         expected_trie["c3/9545/c7070000000f"] = "mov dword ptr [rdi], 0xf000000"
         expected_trie["c3/00000f9545"] = "add byte ptr [rax], al"
 
-        actual_trie = rop_gadget.get_inst_trie()
+        actual_trie = rop_hunter.get_inst_trie()
         self.assertCountEqual(actual_trie.items(), expected_trie.items())
 
         # len(code) = 9
@@ -34,22 +34,22 @@ class ROPTest(unittest.TestCase):
         expected_inst_addr["c3/9545/c7070000000f"] = "0x1000"
         expected_inst_addr["c3/00000f9545"] = "0x1003"
 
-        actual_inst_addr = rop_gadget.get_inst_addr_dict()
+        actual_inst_addr = rop_hunter.get_inst_addr_dict()
         self.assertDictEqual(actual_inst_addr, expected_inst_addr)
 
     def test_multiple_ret(self):
-        rop_gadget = ROPGadget()
+        rop_hunter = ROPHunter()
         start_offset = 0x1000
         code = b"\x58\xc3\x95\x45\xc3"
 
-        rop_gadget.galileo(start_offset, code)
+        rop_hunter.galileo(start_offset, code)
 
         expected_trie = pygtrie.StringTrie()
         expected_trie["c3"] = "ret"
         expected_trie["c3/58"] = "pop rax"
         expected_trie["c3/9545"] = "xchg eax, ebp"
 
-        actual_trie = rop_gadget.get_inst_trie()
+        actual_trie = rop_hunter.get_inst_trie()
         self.assertCountEqual(actual_trie.items(), expected_trie.items())
 
         # len(code) = 9
@@ -58,26 +58,26 @@ class ROPTest(unittest.TestCase):
         expected_inst_addr["c3/58"] = "0x1000"
         expected_inst_addr["c3/9545"] = "0x1002"
 
-        actual_inst_addr = rop_gadget.get_inst_addr_dict()
+        actual_inst_addr = rop_hunter.get_inst_addr_dict()
         self.assertDictEqual(actual_inst_addr, expected_inst_addr)
 
     def verify_boring_inst(self, code):
-        rop_gadget = ROPGadget()
+        rop_hunter = ROPHunter()
         start_offset = 0x1000
 
-        rop_gadget.galileo(start_offset, code)
+        rop_hunter.galileo(start_offset, code)
 
         # ret is a boring instruction
         # ret appears in our trie, because it's the root
         expected_trie = pygtrie.StringTrie()
         expected_trie["c3"] = "ret"
 
-        actual_trie = rop_gadget.get_inst_trie()
+        actual_trie = rop_hunter.get_inst_trie()
         self.assertCountEqual(actual_trie.items(), expected_trie.items())
 
         # We don't keep track of the address of these rets, because it's boring
         expected_inst_addr = dict()
-        actual_inst_addr = rop_gadget.get_inst_addr_dict()
+        actual_inst_addr = rop_hunter.get_inst_addr_dict()
         self.assertDictEqual(actual_inst_addr, expected_inst_addr)
 
     def test_boring_leave_ret(self):
@@ -93,11 +93,11 @@ class ROPTest(unittest.TestCase):
         self.verify_boring_inst(code)
 
     def test_boring_unconditional_jmp(self):
-        rop_gadget = ROPGadget()
+        rop_hunter = ROPHunter()
         start_offset = 0x1000
         code = b"\xE9\xFC\xFF\xFF\xFF\xC3"  # jmp 1 <_main+0x1>; ret
 
-        rop_gadget.galileo(start_offset, code)
+        rop_hunter.galileo(start_offset, code)
 
         # We cannot use verify_boring_inst() even though this instruction is goring.
         # Since this code snippet is long, it finds an additional gadget: cld; ret
@@ -105,13 +105,13 @@ class ROPTest(unittest.TestCase):
         expected_trie["c3"] = "ret"
         expected_trie["c3/fcffffff"] = "cld "
 
-        actual_trie = rop_gadget.get_inst_trie()
+        actual_trie = rop_hunter.get_inst_trie()
         self.assertCountEqual(actual_trie.items(), expected_trie.items())
 
         expected_inst_addr = dict()
         expected_inst_addr["c3/fcffffff"] = "0x1001"
 
-        actual_inst_addr = rop_gadget.get_inst_addr_dict()
+        actual_inst_addr = rop_hunter.get_inst_addr_dict()
         self.assertDictEqual(actual_inst_addr, expected_inst_addr)
 
 if __name__ == '__main__':
